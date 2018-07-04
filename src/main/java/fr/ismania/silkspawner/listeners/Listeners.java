029c3495f22ff7b4a84462ad30aa4012e500c0d3
Aymon <32235434+LucasAymon@users.noreply.github.com>
2018-07-04 22:44:59 +0200

    Update Listeners.java

--- src/main/java/fr/ismania/silkspawner/listeners/Listeners.java
+++ src/main/java/fr/ismania/silkspawner/listeners/Listeners.java
@@ -32,10 +32,41 @@ public class Listeners implements Listener {
 			
 			if(la_permission) {
 				
+				//Vérification du monde
+				if(p.getWorld() == Bukkit.getWorld("ASkyBlock")) {
+				
+				//Vérification du nom de la pioche
+				if(itemInHand.getItemMeta().getDisplayName().equals("§c§lPioche à Spawners") && itemInHand.getItemMeta().getDisplayName() != null) {
+					
+					//Vérification des mobs spawner par le spawner
+					if(spawner.getSpawnedType() == EntityType.CHICKEN || spawner.getSpawnedType() == EntityType.VILLAGER || spawner.getSpawnedType() == EntityType.BLAZE || spawner.getSpawnedType() == EntityType.SKELETON || spawner.getSpawnedType() == EntityType.ZOMBIE || spawner.getSpawnedType() == EntityType.CREEPER || spawner.getSpawnedType() == EntityType.PIG || spawner.getSpawnedType() == EntityType.COW || spawner.getSpawnedType() == EntityType.SPIDER || spawner.getSpawnedType() == EntityType.SQUID || spawner.getSpawnedType() == EntityType.RABBIT || spawner.getSpawnedType() == EntityType.SHEEP || spawner.getSpawnedType() == EntityType.SLIME || spawner.getSpawnedType() == EntityType.WITCH || spawner.getSpawnedType() == EntityType.GUARDIAN) {
+						
+						//Nouvelle item spawner
+						ItemStack mob_spawner = new ItemStack(Material.MOB_SPAWNER);
+						//BlockState de l'item spawner
+						BlockStateMeta mob_spawner_meta = (BlockStateMeta) mob_spawner.getItemMeta();
+						
+						//Creation de la "créature" du spawner
+						CreatureSpawner cSpawner = spawner;
+						cSpawner.setSpawnedType(spawner.getSpawnedType());
+						cSpawner.setCreatureTypeByName(spawner.getCreatureTypeName());
+						
+						//On set le blockstate du spawner
+						mob_spawner_meta.setBlockState(cSpawner);
+						mob_spawner_meta.setDisplayName(cSpawner.getCreatureTypeName() + " Spawner");
+						mob_spawner.setItemMeta(mob_spawner_meta);
+						
+						//On drop l'item du spawner
+						p.getWorld().dropItem(p.getLocation(), mob_spawner);
+						
+					}
+
+				}
+				
 			} else {
 				
 				//Vérification du monde
-			if(p.getWorld() == Bukkit.getWorld("ASkyBlock")) {
+				if(p.getWorld() == Bukkit.getWorld("ASkyBlock")) {
 				
 				//Vérification du nom de la pioche
 				if(itemInHand.getItemMeta().getDisplayName().equals("§c§lPioche à Spawners") && itemInHand.getItemMeta().getDisplayName() != null) {
