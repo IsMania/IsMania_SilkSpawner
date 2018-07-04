bccde5a588f99f86ff78d64210a0149cbdba49bb
Aymon <32235434+LucasAymon@users.noreply.github.com>
2018-07-04 22:57:33 +0200

    Update Listeners.java

--- src/main/java/fr/ismania/silkspawner/listeners/Listeners.java
+++ src/main/java/fr/ismania/silkspawner/listeners/Listeners.java
@@ -16,92 +16,93 @@ public class Listeners implements Listener {
 	@SuppressWarnings("deprecation")
 	@EventHandler
 	public void onBreak(BlockBreakEvent e) {
-		
+
 		Player p = e.getPlayer();
 		//Variable pour le récupérer le material détruit
 		Material detruit = e.getBlock().getType();
 
 		//Vérification si le material détruit est un spawner
 		if(detruit == Material.MOB_SPAWNER) {
-			
+
 			//Recupération du state du block
 			CreatureSpawner spawner = (CreatureSpawner) e.getBlock().getState();
 			ItemStack itemInHand = p.getItemInHand();
-			
+
 			if(itemInHand == null) return;
-			
+
 			if(p.hasPermission("sp.bypass")) {
-				
+
 				//Vérification du nom de la pioche
 				if(itemInHand.getItemMeta().getDisplayName().equals("§c§lPioche à Spawners") && itemInHand.getItemMeta().getDisplayName() != null) {
-					
+
 					//Vérification des mobs spawner par le spawner
 					if(spawner.getSpawnedType() == EntityType.CHICKEN || spawner.getSpawnedType() == EntityType.VILLAGER || spawner.getSpawnedType() == EntityType.BLAZE || spawner.getSpawnedType() == EntityType.SKELETON || spawner.getSpawnedType() == EntityType.ZOMBIE || spawner.getSpawnedType() == EntityType.CREEPER || spawner.getSpawnedType() == EntityType.PIG || spawner.getSpawnedType() == EntityType.COW || spawner.getSpawnedType() == EntityType.SPIDER || spawner.getSpawnedType() == EntityType.SQUID || spawner.getSpawnedType() == EntityType.RABBIT || spawner.getSpawnedType() == EntityType.SHEEP || spawner.getSpawnedType() == EntityType.SLIME || spawner.getSpawnedType() == EntityType.WITCH || spawner.getSpawnedType() == EntityType.GUARDIAN) {
-						
+
 						//Nouvelle item spawner
 						ItemStack mob_spawner = new ItemStack(Material.MOB_SPAWNER);
 						//BlockState de l'item spawner
 						BlockStateMeta mob_spawner_meta = (BlockStateMeta) mob_spawner.getItemMeta();
-						
+
 						//Creation de la "créature" du spawner
 						CreatureSpawner cSpawner = spawner;
 						cSpawner.setSpawnedType(spawner.getSpawnedType());
 						cSpawner.setCreatureTypeByName(spawner.getCreatureTypeName());
-						
+
 						//On set le blockstate du spawner
 						mob_spawner_meta.setBlockState(cSpawner);
 						mob_spawner_meta.setDisplayName(cSpawner.getCreatureTypeName() + " Spawner");
 						mob_spawner.setItemMeta(mob_spawner_meta);
-						
+
 						//On drop l'item du spawner
 						p.getWorld().dropItem(p.getLocation(), mob_spawner);
-						
+
 					}
 				}
-				
+
 			} else {
-				
+
 				//Vérification du monde
 				if(p.getWorld() == Bukkit.getWorld("ASkyBlock")) {
-				
-				//Vérification du nom de la pioche
-				if(itemInHand.getItemMeta().getDisplayName().equals("§c§lPioche à Spawners") && itemInHand.getItemMeta().getDisplayName() != null) {
-					p.getInventory().getItemInHand().setType(Material.AIR);
-					//Vérification des mobs spawner par le spawner
-					if(spawner.getSpawnedType() == EntityType.CHICKEN || spawner.getSpawnedType() == EntityType.VILLAGER || spawner.getSpawnedType() == EntityType.BLAZE || spawner.getSpawnedType() == EntityType.SKELETON || spawner.getSpawnedType() == EntityType.ZOMBIE || spawner.getSpawnedType() == EntityType.CREEPER || spawner.getSpawnedType() == EntityType.PIG || spawner.getSpawnedType() == EntityType.COW || spawner.getSpawnedType() == EntityType.SPIDER || spawner.getSpawnedType() == EntityType.SQUID || spawner.getSpawnedType() == EntityType.RABBIT || spawner.getSpawnedType() == EntityType.SHEEP || spawner.getSpawnedType() == EntityType.SLIME || spawner.getSpawnedType() == EntityType.WITCH || spawner.getSpawnedType() == EntityType.GUARDIAN) {
-						
-						//Nouvelle item spawner
-						ItemStack mob_spawner = new ItemStack(Material.MOB_SPAWNER);
-						//BlockState de l'item spawner
-						BlockStateMeta mob_spawner_meta = (BlockStateMeta) mob_spawner.getItemMeta();
-						
-						//Creation de la "créature" du spawner
-						CreatureSpawner cSpawner = spawner;
-						cSpawner.setSpawnedType(spawner.getSpawnedType());
-						cSpawner.setCreatureTypeByName(spawner.getCreatureTypeName());
-						
-						//On set le blockstate du spawner
-						mob_spawner_meta.setBlockState(cSpawner);
-						mob_spawner_meta.setDisplayName(cSpawner.getCreatureTypeName() + " Spawner");
-						mob_spawner.setItemMeta(mob_spawner_meta);
-						
-						//On drop l'item du spawner
-						p.getWorld().dropItem(p.getLocation(), mob_spawner);
-						
+
+					//Vérification du nom de la pioche
+					if(itemInHand.getItemMeta().getDisplayName().equals("§c§lPioche à Spawners") && itemInHand.getItemMeta().getDisplayName() != null) {
+						p.getInventory().getItemInMainHand().setType(Material.AIR);
+						//Vérification des mobs spawner par le spawner
+						if(spawner.getSpawnedType() == EntityType.CHICKEN || spawner.getSpawnedType() == EntityType.VILLAGER || spawner.getSpawnedType() == EntityType.BLAZE || spawner.getSpawnedType() == EntityType.SKELETON || spawner.getSpawnedType() == EntityType.ZOMBIE || spawner.getSpawnedType() == EntityType.CREEPER || spawner.getSpawnedType() == EntityType.PIG || spawner.getSpawnedType() == EntityType.COW || spawner.getSpawnedType() == EntityType.SPIDER || spawner.getSpawnedType() == EntityType.SQUID || spawner.getSpawnedType() == EntityType.RABBIT || spawner.getSpawnedType() == EntityType.SHEEP || spawner.getSpawnedType() == EntityType.SLIME || spawner.getSpawnedType() == EntityType.WITCH || spawner.getSpawnedType() == EntityType.GUARDIAN) {
+
+							//Nouvelle item spawner
+							ItemStack mob_spawner = new ItemStack(Material.MOB_SPAWNER);
+							//BlockState de l'item spawner
+							BlockStateMeta mob_spawner_meta = (BlockStateMeta) mob_spawner.getItemMeta();
+
+							//Creation de la "créature" du spawner
+							CreatureSpawner cSpawner = spawner;
+							cSpawner.setSpawnedType(spawner.getSpawnedType());
+							cSpawner.setCreatureTypeByName(spawner.getCreatureTypeName());
+
+							//On set le blockstate du spawner
+							mob_spawner_meta.setBlockState(cSpawner);
+							mob_spawner_meta.setDisplayName(cSpawner.getCreatureTypeName() + " Spawner");
+							mob_spawner.setItemMeta(mob_spawner_meta);
+
+							//On drop l'item du spawner
+							p.getWorld().dropItem(p.getLocation(), mob_spawner);
+							p.getInventory().remove(itemInHand);
+
+						}
+
 					}
 
+				} else {
+					p.sendMessage("§cTu ne peux pas récupérer de spawners dans ce monde !");
+					e.setCancelled(true);
 				}
 
-			} else {
-				p.sendMessage("§cTu ne peux pas récupérer de spawners dans ce monde !");
-				e.setCancelled(true);
-			}
-				
 			}
 
 		}
-		
+
 	}
-	
+
 }
 
