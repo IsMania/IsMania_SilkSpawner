73ff03d8573b3aba8076dc9fffe03be12cb1318e
Lucas <aymonlucas@gmail.com>
2018-07-05 22:44:33 +0200

    Update remove pickaxe

--- src/main/java/fr/ismania/silkspawner/listeners/Listeners.java
+++ src/main/java/fr/ismania/silkspawner/listeners/Listeners.java
@@ -40,7 +40,7 @@ public class Listeners implements Listener {
 						//Vérification des mobs spawner par le spawner
 						if(spawner.getSpawnedType() == EntityType.CHICKEN || spawner.getSpawnedType() == EntityType.VILLAGER || spawner.getSpawnedType() == EntityType.BLAZE || spawner.getSpawnedType() == EntityType.SKELETON || spawner.getSpawnedType() == EntityType.ZOMBIE || spawner.getSpawnedType() == EntityType.CREEPER || spawner.getSpawnedType() == EntityType.PIG || spawner.getSpawnedType() == EntityType.COW || spawner.getSpawnedType() == EntityType.SPIDER || spawner.getSpawnedType() == EntityType.SQUID || spawner.getSpawnedType() == EntityType.RABBIT || spawner.getSpawnedType() == EntityType.SHEEP || spawner.getSpawnedType() == EntityType.SLIME || spawner.getSpawnedType() == EntityType.WITCH || spawner.getSpawnedType() == EntityType.GUARDIAN) {
 							
-							if(p.getInventory().contains(itemInHand)) p.getInventory().remove(itemInHand);
+							if(p.getInventory().contains(itemInHand)) p.getInventory().removeItem(itemInHand);
 							
 						}
 
